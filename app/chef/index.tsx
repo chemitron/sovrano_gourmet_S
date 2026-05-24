b5fb33a0705dd2f6ff01bb949206d3fa47722278
chemitron <67175035+chemitron@users.noreply.github.com>
2026-05-24 15:12:47 -0400

    removed duplicated navigation from chef to lista de compras

--- app/chef/index.tsx
+++ app/chef/index.tsx
@@ -128,12 +128,6 @@ useEffect(() => {
                         onPress={() => router.push("/empleado")}
                       />
 </View> 
-<View style={{ paddingBottom: 20 }}>
-          <Button_style2
-                        title="Compras pendientes"
-                        onPress={() => router.push("/administrador/reports/comprasNecesarias")}
-                      />
-</View> 
           <Button_style2
                       title="Cerrar sesión"
                       onPress={handleLogout}
