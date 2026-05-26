600a0625a0e0dfc2b5a68159a55958adf707f577
chemitron <67175035+chemitron@users.noreply.github.com>
2026-05-25 21:17:34 -0400

    fixed date format

--- app/administrador/reports/ventas.tsx
+++ app/administrador/reports/ventas.tsx
@@ -29,8 +29,11 @@ export default function ReportsVentas() {
   const [valorCombinado, setValorCombinado] = useState<number | null>(null);
 
   const formatLocalDate = (date: Date) => {
-    return date.toISOString().split("T")[0];
-  };
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
 
   const generarReporte = async () => {
     const start = new Date(`${formatLocalDate(fechaInicio)}T00:00:00`);
